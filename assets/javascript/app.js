27ca8ecd53d872c5a95f42af6c5113831222a7fc
John Coleman <johncoleman_2@hotmail.com>
2018-02-13 17:26:24 -0600

    initial load

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+var number = 100;
+
+var intervalId;
+
+$("#stop").on("click", stop);
+
+$("#resume").on("click", run);
+
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+
+    number--;
+
+    $("#show-number").html("<h2>" + number + "</h2>");
+
+    if (number === 0) {
+
+        stop();
+
+        alert("Time Up!");
+    }
+}
+
+function stop() {
+
+    clearInterval(intervalId);
+}
+
+run();
